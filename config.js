5d379c9529e0632aedf3bf8ea03db69df13640c6
Yousra.Bokra <yousrabokra.tech@gmail.com>
2026-02-22 21:43:41 +0500

    uplode code

--- config.js
+++ config.js
@@ -1,9 +1,23 @@
+// import {createClient} from "https://esm.sh/@supabase/supabase-js@2" 
+// console.log(createClient);
+
+// const supabaseUrl = "https://aeplykziuvcsmkrsmbkm.supabase.co"
+
+// const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFlcGx5a3ppdXZjc21rcnNtYmttIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjcwMTcwMzMsImV4cCI6MjA4MjU5MzAzM30.ghcjUh-hux39YzkBhXB2XvzpsJY0x2XlFHP-UHM2314"
+
+// const supabase = createClient(supabaseUrl,supabaseKey);
+// console.log(supabase);
+
+
+// export default supabase;
+
+
 import {createClient} from "https://esm.sh/@supabase/supabase-js@2" 
 console.log(createClient);
 
-const supabaseUrl = "https://aeplykziuvcsmkrsmbkm.supabase.co"
+const supabaseUrl = "https://peuzresauedlpvamhkdx.supabase.co"
 
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFlcGx5a3ppdXZjc21rcnNtYmttIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjcwMTcwMzMsImV4cCI6MjA4MjU5MzAzM30.ghcjUh-hux39YzkBhXB2XvzpsJY0x2XlFHP-UHM2314"
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBldXpyZXNhdWVkbHB2YW1oa2R4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjYwMzkwNDMsImV4cCI6MjA4MTYxNTA0M30.lL237qndDHVf4s7uF931ho-N162SiWiQfuYgsNqDkkg"
 
 const supabase = createClient(supabaseUrl,supabaseKey);
 console.log(supabase);
